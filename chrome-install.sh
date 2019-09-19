0c273207c5e265d7f69c2a4e6a2c93a48a1acdc6
Pritesh Kumar <pritesh.kumar2@gmail.com>
2019-09-19 17:48:49 +0530

    add chrome changes

--- chrome-install.sh
+++ chrome-install.sh
@@ -1,5 +1,6 @@
-#sudo rm /var/lib/dpkg/lock-frontend
-#sudo dpkg --configure -a
+sudo rm /var/lib/dpkg/lock-frontend
+#sudo rm /var/lib/dpkg/lock
+sudo dpkg --configure -a
 #wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 #sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
 sudo apt-get update -y
