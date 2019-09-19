46d635d33dc8e459db6c9648002e84d21984a4cb
Pritesh Kumar <pritesh.kumar2@gmail.com>
2019-09-19 16:53:26 +0530

    add chrome changes

--- chrome-install.sh
+++ chrome-install.sh
@@ -1,4 +1,4 @@
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
-sudo apt-get update -y
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+#sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+#sudo apt-get update -y
 sudo apt-get install -y google-chrome-stable 
