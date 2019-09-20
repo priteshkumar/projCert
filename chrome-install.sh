6ed262fb83e93ec69b5d6f4e108fe4beee921dce
Pritesh Kumar <pritesh.kumar2@gmail.com>
2019-09-20 18:52:38 +0530

    remove some files

--- chrome-install.sh
+++
@@ -1,9 +0,0 @@
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/lib/dpkg/lock
-sudo rm /var/cache/apt/archives/lock
-sudo rm /var/lib/apt/lists/lock
-sudo dpkg --configure -a
-#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-#sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
-sudo apt-get update -y
-sudo apt-get install -y google-chrome-stable 
